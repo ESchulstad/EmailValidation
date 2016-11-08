453fc1ac49969cacb4e535833a8924b4e6776327
ESchulstad <eschulstad@gmail.com>
2016-11-07 21:08:58 -0500

    initial commit

---
+++ EmailValidation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace EmailValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string file = "foo@bar.com";
+
+            if(file == @"(.*)\@.*\.com")
+            {
+
+            }
+        }
+    }
+}
